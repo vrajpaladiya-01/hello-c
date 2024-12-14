127464f82569732e7fa626916fee85b68c01b77a
vrajpaladiya-01 <vrajpaladiya01@gmail.com>
2024-12-14 14:25:24 +0530

    Add files via upload
    how to work int value, float value & char value

---
+++ IFC.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int age = 22;
+    printf("age is : %d \n", age);
+
+    float pi = 3.14;
+    printf("pi is : %f \n", pi);
+
+    char dollar = '$';
+    printf("dollar is : %c \n", dollar);
+
+    return 0;
+}
